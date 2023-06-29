a167c242d29b16e98d4e9c9dfccd04079b451036
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-06-30 01:11:45 +0700

    update

--- pading/pro1/app2.js
+++ pading/pro1/app2.js
@@ -1,29 +1,29 @@
 const body = document.querySelector("body");
-const sidebar = document.querySelector(".sidebar");
+const listMenu = document.querySelector(".list-menu");
 const submenuItems = document.querySelectorAll(".submenu_item");
-sidebar.addEventListener("mouseenter", () => {
-  if (sidebar.classList.contains("hoverable")) {
-    sidebar.classList.remove("close");
-  }
+listMenu.addEventListener("mouseenter", () => {
+    if (listMenu.classList.contains("hoverable")) {
+        listMenu.classList.remove("close");
+    }
 });
-sidebar.addEventListener("mouseleave", () => {
-  if (sidebar.classList.contains("hoverable")) {
-    sidebar.classList.add("close");
-  }
+listMenu.addEventListener("mouseleave", () => {
+    if (listMenu.classList.contains("hoverable")) {
+        listMenu.classList.add("close");
+    }
 });
 submenuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("show_submenu");
-    submenuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show_submenu");
-      }
+    item.addEventListener("click", () => {
+        item.classList.toggle("show_submenu");
+        submenuItems.forEach((item2, index2) => {
+            if (index !== index2) {
+                item2.classList.remove("show_submenu");
+            }
+        });
     });
-  });
 });
 
 if (window.innerWidth < 768) {
-  sidebar.classList.add("close");
+    listMenu.classList.add("close");
 } else {
-  sidebar.classList.remove("close");
+    listMenu.classList.remove("close");
 }
--- pading/pro1sss/app.js
+++ pading/pro1sss/app.js
@@ -1,8 +1,8 @@
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
+    toggle = body.querySelector('#toggle'),
+    // searchBtn = body.querySelector(".search-box"),
+    // modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
 
@@ -10,17 +10,33 @@ toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-})
 
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-
+//
+const listMenu = document.querySelector(".list-menu");
+const submenuItems = document.querySelectorAll(".submenu_item");
+listMenu.addEventListener("mouseenter", () => {
+    if (listMenu.classList.contains("hoverable")) {
+        listMenu.classList.remove("close");
     }
 });
+listMenu.addEventListener("mouseleave", () => {
+    if (listMenu.classList.contains("hoverable")) {
+        listMenu.classList.add("close");
+    }
+});
+submenuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("show_submenu");
+        submenuItems.forEach((item2, index2) => {
+            if (index !== index2) {
+                item2.classList.remove("show_submenu");
+            }
+        });
+    });
+});
+
+if (window.innerWidth < 768) {
+    listMenu.classList.add("close");
+} else {
+    listMenu.classList.remove("close");
+}
