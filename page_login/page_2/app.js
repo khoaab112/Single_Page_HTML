57d5ec2a54007058352a9a10e988e537978e2934
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-07-13 17:26:43 +0700

    update login_v2

---
+++ page_login/page_2/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var fullHeight = function() {
+  var fullheightElements = document.querySelectorAll('.js-fullheight');
+  var resizeHandler = function() {
+    fullheightElements.forEach(function(element) {
+      element.style.height = window.innerHeight + 'px';
+    });
+  };
+  
+  resizeHandler();
+  window.addEventListener('resize', resizeHandler);
+};
+
+fullHeight();
+
+var togglePassword = function() {
+  var toggleButtons = document.querySelectorAll('.toggle-password');
+  toggleButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      this.classList.toggle('fa-eye');
+      this.classList.toggle('fa-eye-slash');
+      
+      var input = document.querySelector(button.getAttribute('toggle'));
+      if (input.getAttribute('type') === 'password') {
+        input.setAttribute('type', 'text');
+      } else {
+        input.setAttribute('type', 'password');
+      }
+    });
+  });
+};
+
+togglePassword();
